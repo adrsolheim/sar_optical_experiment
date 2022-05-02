ba664c5ca425259fa369ca50ae2b36bc137e4f8d
adrsolheim <adrsolheim2@gmail.com>
2022-05-02 13:09:11 +0200

    update

---
+++ gini.R
@@ -0,0 +1,13 @@
+library(raster)
+library(leafR)
+library(lidR)
+#import::from(leafR, GC)
+
+
+
+las_file = system.file("mockdata", "normalized-47.las", package="lidR")
+#las = readLAS(las_file, select = "*", filter = "")
+
+#GC(las, threshold =1)
+
+
